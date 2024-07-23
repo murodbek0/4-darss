ae9be25fb0ee9a881e74210402e42dd83602bef0
Murodbek0 <tursunaliyevmurodbek07@gmail.com>
2024-07-23 15:46:57 +0500

    4 dars

---
+++ js/index.js
@@ -0,0 +1,159 @@
+// for
+// 1 misol
+// let sum = 0;
+// let res = '';
+// for( let i =10; i <= 100; i++){
+//    if( i % 2 !== 0);
+//    sum = sum + i ;
+// }
+// console.log(sum);
+
+// 2 misol
+// let n = +prompt("Sonni kiriting");
+// let sum = 1;
+
+// for (let i = 1; i <= n; i++) {
+//   sum *= i;
+//    console.log(sum);
+// }
+// 3 misol
+
+// 4 misol
+// let sum = 1;
+// for (let i = 1 ; i <= 20; i++) {
+// sum = sum * 2;
+
+// }
+// console.log(sum);
+
+// 5 misol
+
+// let numbers = [];
+// for (let i = 0; i < 5; i++) {
+//   let num = parseFloat(prompt("son krting"));
+//   numbers.push(num);
+// }
+// let max = numbers[0];
+// let min = numbers[0];
+
+// for (let i = 1; i < numbers.length; i++) {
+//   if (numbers[i] > max) {
+//     max = numbers[i];
+//   }
+
+//   if (numbers[i] > min) {
+//     min = numbers[i];
+//   }
+// }
+// console.log(max);
+// console.log(min);
+
+// 6 misol
+ 
+// 7 misol
+
+// let sum = prompt('son krting');
+// let reversedNumber = '';
+// for( let i = number.length - 1; i >=0; i--){
+//     reversedNumber += number[i];
+
+// }
+// console.log(reversedNumber  );
+// 8 misol
+// let sum = 0;
+// let sum1 = 0;
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 == 0) {
+//     sum += i * i;
+//   } else {
+//     sum1 += i * i * i;
+//   }
+// }
+// let difference = sum - sum1;
+// console.log(sum);
+// console.log(sum1);
+// console.log(farqi);
+
+// 9 misol
+
+// for (let i = 1; i <= 50; i++) {
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log(i);
+//   }
+// }
+// 10 misol
+
+// let sum = 0;
+// let count = 100;
+// for(let i = 1; i <=100; i++){
+
+// sum =sum + i
+
+// }
+// let res = sum / count;
+// console.log(res);
+
+// 11 misol
+
+// let sum = prompt("son krting");
+// let digitCount = 0;
+// for (let i = 0; i < number.length; i++) {
+//   if (!isNaN(number[i])) {
+//     digitCount++;
+//   }
+// }
+// console.log(digitCount);
+
+// 12 misol
+
+
+// break  continue =====
+
+// 1 misol
+
+// for (let i = ; i <= 10; i++) {
+//   if (i === 5){
+//  break;
+
+// }
+// console.log(i);
+// }
+
+// 2 misol
+
+// for (let i = ; i <= 10; i++) {
+//   if (i === 3 || i === 7){
+//  continue;
+
+// }
+// console.log(i);
+// }
+
+// 3 misol
+// for (let i = ; i <= 10; i++) {
+//   if (i === 7){
+
+//  break;
+
+// }
+// console.log(i);
+// }
+// 4 misol
+
+// for (let i = ; i <= 10; i++) {
+//   if (i % 2 == 0 ){
+//  continue;
+
+// }
+// console.log(i);
+// }
+
+// 5 misol
+
+// for (let i = ; i <= 100; i++) {
+//   if (i === 50){
+//  break;
+
+// }
+// console.log(i);
+// }
